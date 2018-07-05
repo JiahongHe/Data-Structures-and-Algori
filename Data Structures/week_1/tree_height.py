c7253dcf18ee644d0d8e7d962f4d2f1d9eee531b
Jiahong He <jh3863@columbia.edu>
2018-07-05 16:41:24 +0800

    course2

--- Data Structures/week_1/tree_height.py
+++ Data Structures/week_1/tree_height.py
@@ -5,38 +5,32 @@ sys.setrecursionlimit(10**7) # max depth of recursion
 threading.stack_size(2**27)  # new thread will get stack of such size
 
 class TreeHeight:
-        def read(self):
-                self.n = int(sys.stdin.readline())
-                self.parent = list(map(int, sys.stdin.readline().split()))
+    def __init__(self):
+        self.n = 0
+        self.parent = []
+        self.cache = []
 
-        def compute_height_original(self):
-                maxHeight = 0
-                for vertex in range(self.n):
-                        height = 0
-                        i = vertex
-                        while i != -1:
-                                height += 1
-                                i = self.parent[i]
-                        maxHeight = max(maxHeight, height);
-                return maxHeight;
+    def read(self):
+        self.n = int(sys.stdin.readline())
+        self.parent = list(map(int, sys.stdin.readline().split()))
 
-        def path_len(self, node_id):
-            parent = self.parent[node_id]
-            if parent == -1:
-                return 1
+    def path_len(self, node_id):
+        parent = self.parent[node_id]
+        if parent == -1:
+            return 1
 
-            if self.cache[node_id]:
-                return self.cache[node_id]
-
-            self.cache[node_id] = 1 + self.path_len(self.parent[node_id])
+        if self.cache[node_id]:
             return self.cache[node_id]
 
-        def compute_height_new(self):
-            return max([self.path_len(i) for i in range(self.n)])
+        self.cache[node_id] = 1 + self.path_len(self.parent[node_id])
+        return self.cache[node_id]
+
+    def compute_height(self):
+        return max([self.path_len(i) for i in range(self.n)])
 
 def main():
   tree = TreeHeight()
   tree.read()
-  print(tree.compute_height_new())
+  print(tree.compute_height())
 
 threading.Thread(target=main).start()
